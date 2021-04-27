fec7a86109a82c3845ed426529147127fee1058e
Valdeth <alstontravis6@gmail.com>
2021-04-27 00:34:38 -0600

    models added

--- models/Category.js
+++ models/Category.js
@@ -11,9 +11,13 @@ Category.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
-  },
+    },
   {
     sequelize,
     timestamps: false,
